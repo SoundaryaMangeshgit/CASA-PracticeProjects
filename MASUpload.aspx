c38e4992fa064ee2e05b37abc97f43b58f4c12cc
SoundaryaMangeshgit <69762300+SoundaryaMangeshgit@users.noreply.github.com>
2020-11-20 17:51:16 +0530

    Add files via upload

---
+++ MASUpload.aspx
@@ -0,0 +1,98 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MASUpload.aspx.cs" Inherits="MASUploadAutomateScript.MASUpload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 226px;
+        }
+        .auto-style2 {
+            margin-right: 3px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="OuterDiv">
+            <table id="tblOuter">
+       <tr>
+            <td>
+                <strong>1.Upload text File
+             </strong>
+             </td>
+           <td class="auto-style1" >
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+               </td>
+          
+           <td>
+               <asp:Label ID="lblFileUpload" runat="server" visible="false" Text="Label"></asp:Label>
+           </td>
+            </tr>
+                
+
+                <tr>
+                    <td class="auto-style1">
+                        <strong> 2.Enter the Archive Drive path</strong>
+
+                    </td>
+                    <td>
+                        <asp:TextBox ID="tbArchiveDrivePath" runat="server"></asp:TextBox>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                         <strong> 3.Enter the path where CCUser wants it to be placed</strong>
+                         
+                        </td>
+                    <td>
+                        <asp:TextBox ID="tbUserPath" runat="server"></asp:TextBox>
+                    </td>
+
+                </tr>
+
+                <tr>
+                    <td>
+                         <strong> 4.Enter the name of output file</strong>
+                        
+                        </td>
+                    <td>
+                        <asp:TextBox ID="tbOutputfile" runat="server"></asp:TextBox>
+                    </td>
+
+                </tr>
+                <tr>
+                    <td>
+    
+        <asp:Button ID="Button1" runat="server" Text="UploadFile" OnClick="Generate_DAT_File" />
+                        <asp:Label ID="lblWriteStatus" Visible="false" runat="server" Text="Label"></asp:Label>
+                        </td>
+
+                </tr>
+                <tr>
+                        <td>
+                         <strong> Enter the command to run on Power Shell</strong>
+                            </td>
+                    <td>
+                            <asp:TextBox ID="tbRunScript" runat="server"></asp:TextBox>
+                        
+                        </td>
+                    <td>
+                        <asp:Button ID="btnRunCommand" runat="server" Text="RunScript" OnClick="Run_Power_Command" />
+                    </td>
+                </tr>
+                </table>
+            <table>
+                <tr>
+                    <td>
+                        <asp:TextBox ID="tbDisplayScript" runat="server" BackColor="#3333FF" ForeColor="White" Height="483px" TextMode="MultiLine" Width="800px" CssClass="auto-style2"></asp:TextBox>
+                        </td>
+                </tr>
+                </table>
+             </div>
+    </form>
+</body>
+</html>
---
+++ MASUpload.aspx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static System.Net.WebRequestMethods;
+using File = System.IO.File;
+using System.Management.Automation.Runspaces;
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MASUploadAutomateScript
+{
+    public partial class MASUpload : System.Web.UI.Page
+    {
+        private string MASADPath
+        {
+            get
+            {
+                return this.tbArchiveDrivePath.Text;
+            }
+        }
+
+        private string MASUserPath
+        {
+            get
+            {
+                return this.tbUserPath.Text;
+            }
+        }
+
+        private string MASOutputFile
+        {
+            get
+            {
+                return this.tbOutputfile.Text;
+            }
+        }
+
+        private string MASCommand
+        {
+            get
+            {
+                return this.tbRunScript.Text;
+            }
+        }
+
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+				if (!Page.IsPostBack)
+				{
+				}
+        }
+
+        protected void Generate_DAT_File(object sender, EventArgs Exception)
+        {
+            FileUpload1.SaveAs(Server.MapPath("~/Uploads/" + FileUpload1.FileName));
+            lblFileUpload.Text = "FileUploadedSuccessly";
+
+            try {
+                List<string> ReadFile = File.ReadAllLines(@"FilePath" + FileUpload1.FileName).ToList();
+               
+                StreamWriter sw = new StreamWriter("C:\\path\\"+ MASOutputFile + ".dat");
+            foreach (string line in ReadFile)
+            {
+                String write1 = MASUserPath+"," + MASADPath + line;
+                sw.WriteLine(write1);
+            }
+            sw.Close();
+        }
+            catch(Exception e )
+            {
+                Console.WriteLine("Exception"+ e.Message);
+            }
+            finally
+            {
+                lblWriteStatus.Text = "Output file written successly";
+            }
+        }
+
+        protected void Run_Power_Command(object sender, EventArgs e)
+        {
+            tbDisplayScript.Text = RunCommand(MASCommand);
+        }
+
+        protected string RunCommand(String Command)
+        {
+           
+
+            Runspace runspace = RunspaceFactory.CreateRunspace();
+
+            // open it
+
+            runspace.Open();
+
+            // create a pipeline and feed it the script text
+
+            Pipeline pipeline = runspace.CreatePipeline();
+            pipeline.Commands.AddScript(Command);
+
+
+            pipeline.Commands.Add("Out-String");
+
+            // execute the script
+
+            Collection<PSObject> results = pipeline.Invoke();
+
+            // close the runspace
+
+            runspace.Close();
+
+            // convert the script result into a single string
+
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (PSObject obj in results)
+            {
+                stringBuilder.AppendLine(obj.ToString());
+            }
+
+            return stringBuilder.ToString();
+
+
+        }
+    }
+}
